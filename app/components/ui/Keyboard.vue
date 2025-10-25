070a471dbf26e8bfca4105cf98c93e08f882bc50
Margherita <margherita.magatti@cofidis.it>
2025-10-25 23:20:10 +0200

    change keyboard layout

--- app/components/ui/Keyboard.vue
+++ app/components/ui/Keyboard.vue
@@ -9,11 +9,11 @@
         <div class="grid grid-cols-4 gap-1">
             <UiKeyboardButton char="[" @click="$emit('click', '[')"></UiKeyboardButton>
             <UiKeyboardButton char="]" @click="$emit('click', ']')"></UiKeyboardButton>
-            <UiKeyboardButton char="<" @click="$emit('click', '<')"></UiKeyboardButton>
             <UiKeyboardButton char=">" @click="$emit('click', '>')"></UiKeyboardButton>
+            <UiKeyboardButton char="del" @click="$emit('click', 'del')"></UiKeyboardButton>
         </div>
         <div class="flex gap-1 h-[48px]">
-            <UiKeyboardButton char="del" @click="$emit('click', 'del')"></UiKeyboardButton>
+            <UiKeyboardButton char="<" @click="$emit('click', '<')" class="w-[56px]"></UiKeyboardButton>
             <UiKeyboardButton char=" " @click="$emit('click', 'SPACE')" class="flex-grow"></UiKeyboardButton>
             <UiKeyboardButton char="\n" @click="$emit('click', '\n')"></UiKeyboardButton>
         </div>
