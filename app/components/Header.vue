ee23a542a032efb436dcc3e0c94c3f6f13b7b406
Margherita <margherita.magatti@cofidis.it>
2025-10-25 12:32:35 +0200

    header

---
+++ app/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="pt-[22px] px-6">
+        <div class="flex justify-between w-full ">
+            <UiButtonSmall :text="'<>'" :priority="2" />
+            <div class="text-center">
+                <h1 class="font-mono leading-[100%]">
+                    Esolang <br/>Playground
+                </h1>
+            </div>
+            <UiButtonSmall :text="'<3'" :priority="2" />
+        </div>
+        <h2 class="mt-1 text-center">
+            Brainfuck
+        </h2>
+    </header>
+</template>
+<script setup lang="ts">
+</script>
---
+++ app/components/ui/ButtonSmall.vue
@@ -0,0 +1,16 @@
+<template>
+    <button type="button" class="text-[14px]">
+        <span 
+            class="block font-white border-1  rounded-[3px] px-3 py-1 "
+            :class="priority === 1 ? 'bg-purple-500 border-purple-300 shadow-(--button-dark-shadow)' : 'bg-dark-700 border-purple-300/50 shadow-(--button-2-dark-shadow)' "
+        >
+            {{ text }}
+        </span>
+    </button>
+</template>
+<script setup lang="ts">
+defineProps({
+    text: {type: String, required: true},
+    priority: {type: Number, default: 1}
+})
+</script>
