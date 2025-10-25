74a90bd006dc7eb3fcd4d9f9e0e6e1885f968341
Margherita <margherita.magatti@cofidis.it>
2025-10-25 23:19:58 +0200

    button big

---
+++ app/components/ui/ButtonBig.vue
@@ -0,0 +1,16 @@
+<template>
+    <button type="button" class="text-[14px]">
+        <span 
+            class="block font-white font-semibold border rounded-[3px] px-4 py-1.5"
+            :class="priority === 1 ? 'bg-purple-500 border-purple-300 shadow-button-dark' : 'bg-dark-700 border-purple-300/50 shadow-button-2-dark' "
+        >
+            {{ text }}
+        </span>
+    </button>
+</template>
+<script setup lang="ts">
+defineProps({
+    text: {type: String, required: true},
+    priority: {type: Number, default: 1}
+})
+</script>
