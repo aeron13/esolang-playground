b9eca53bd3129db1c356fe6396667ca0bf1f0cf8
Margherita <margherita.magatti@cofidis.it>
2025-10-25 23:35:23 +0200

    responsive ascii chart

--- app/components/AsciiChart.vue
+++ app/components/AsciiChart.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="px-6 h-screen w-full absolute z-[99] top-0 left-0 bg-dark-900" >
+    <div class="px-6 h-screen w-full absolute z-[99] top-0 left-0 bg-dark-900 flex flex-col" >
         <div class="w-full flex justify-end pt-[22px]">
             <UiButtonSmall :text="'><'" @click="$emit('close')" />
         </div>
-        <div class="overflow-scroll w-full h-full relative">
-            <h2 class="font-semibold mb-8 text-lg">ASCII table</h2>
-            <div class="mb-6" v-for="item, key in ascii">
+        <div class="overflow-scroll w-full flex-grow relative">
+            <h2 class="font-semibold pb-8 text-lg">ASCII table</h2>
+            <div class="pb-6" v-for="item, key in ascii">
                 <h3 class="mb-2 italic text-sm">{{ key }}</h3>
-                <div class="grid grid-cols-3 text-center w-full text-sm">
+                <div class="grid grid-cols-3 lg:grid-cols-4 text-center w-full text-sm">
                     <p v-for="number, char in item">
                         <span class="font-mono text-xs">{{ char }}</span>: 
                         <span>{{ number }}</span>
