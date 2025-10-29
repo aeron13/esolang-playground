d5669297fca62721a366f311699b8c5600dfcf23
Margherita <margherita.magatti@cofidis.it>
2025-10-29 10:02:23 +0100

    install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  modules: ['@pinia/nuxt'],
 })
