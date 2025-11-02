cce838da7a3348359cc11ab0cd67c29111a2b2f3
Margherita <margherita.magatti@cofidis.it>
2025-11-02 13:11:46 +0100

    test step by step

--- bf-compiler/src/test.ts
+++ bf-compiler/src/test.ts
@@ -4,6 +4,7 @@ class Test {
 
     program: string
     description: string
+    compiler: Compiler
     input: string|null
     output: string|null
     expectedResult: string
@@ -14,23 +15,38 @@ class Test {
         this.description = description
         this.input = input
         this.expectedResult = expected
-    } 
+
+        console.log(this.description)
+        this.compiler = new Compiler(this.program)
+        this.compiler.setInput(this.input)
+    }
+
+    private outputResults() {
+      console.log(`Expected: ${this.expectedResult} \nResult: ${this.output}`)
+      if (this.passed) {
+          console.log('\x1b[32m%s\x1b[0m', 'Test passed\n')
+      } else {
+          console.log('\x1b[31m%s\x1b[0m', 'Test failed\n')
+      }
+    }
+
+    public runStepByStep() {
+      this.compiler.checkCode()
+      let pointer = 0;
+      while (pointer < this.program.length) {
+        const { next } = this.compiler.runNextChar()
+        pointer = next
+      }
+      this.output = this.compiler.output?.join('')
+      this.passed = this.expectedResult === this.output
+      this.outputResults()
+    }
 
     public run() {
-        console.log(this.description)
-        const compiler = new Compiler(this.program)
-        compiler.setInput(this.input)
-        compiler.compile()
-        this.output = compiler.output?.join('')
-
-        console.log(`Expected: ${this.expectedResult} \nResult: ${this.output}`)
+        this.compiler.compile()
+        this.output = this.compiler.output?.join('')
         this.passed = this.expectedResult === this.output
-
-        if (this.passed) {
-            console.log('\x1b[32m%s\x1b[0m', 'Test passed\n')
-        } else {
-            console.log('\x1b[31m%s\x1b[0m', 'Test failed\n')
-        }
+        this.outputResults()
     }
 
 }
@@ -72,3 +88,4 @@ const exampleProgram2 = `>,
   > ++ . >
 ]`
 new Test(exampleProgram2, '2) It: evaluates input correctly', '<', '15').run()
+new Test(exampleProgram2, '3) It: outputs correctly when running step by step', '<', '15').runStepByStep()
