9d34b0a429558a506ffce3e69b33cbf19e2aac6b
Margherita <margherita.magatti@cofidis.it>
2025-11-05 18:00:58 +0100

    implement login form with validation and Firebase authentication

---
+++ app/pages/login.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="px-5 h-full py-4">
+        <UiTabs 
+            :tabs="[{ title: 'Login'}]"
+        >
+            <template #tab1>
+                <div class="px-6 py-6">
+                    <h1 class="block font-mono text-lg max-w-[200px] pt-12">Login</h1>
+                    <form @submit.prevent="handleSubmit" class="flex flex-col gap-5 mt-6" autocomplete="on" autocorrect="on">
+                        <UiInputField 
+                            type="email" 
+                            name="email" 
+                            label="Email" 
+                            v-model="email" 
+                            :disabled="isFormDisabled"
+                            :show-error="v$.email.$dirty && v$.email.$error" 
+                            :error-message="v$.email.$errors[0]?.$message as string" 
+                        />
+                        <UiInputField 
+                            type="password" 
+                            name="password" 
+                            label="Password" 
+                            v-model="password"
+                            :disabled="isFormDisabled"
+                            :show-error="v$.password.$dirty && v$.password.$error" 
+                            :error-message="v$.password.$errors[0]?.$message as string" 
+                        />
+                        <div v-if="error" class="text-orange-code">{{ error }}</div>
+                        <UiButtonBig class="mt-4" type="submit" text="Login" :disabled="isFormDisabled" />
+                        <div v-if="successMessage" class="text-green-400 text-sm">{{ successMessage }}</div>
+                    </form>
+                </div>
+            </template>
+        </UiTabs>
+    </div>
+</template>
+<script setup lang="ts">
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import useVuelidate from '@vuelidate/core'
+import { required, email as emailValidator, helpers } from '@vuelidate/validators'
+
+const email = ref('');
+const password = ref('');
+const error = ref('');
+const isSubmitting = ref(false)
+const isSubmittedSuccessfully = ref(false)
+const successMessage = ref('')
+const isFormDisabled = computed(() => isSubmitting.value || isSubmittedSuccessfully.value)
+
+const rules = computed(() => ({
+    email: {
+        required: helpers.withMessage('Email is required', required),
+        email: helpers.withMessage('Enter a valid email address', emailValidator)
+    },
+    password: {
+        required: helpers.withMessage('Password is required', required)
+    }
+}))
+
+const v$ = useVuelidate(rules, { email, password })
+
+onMounted(() => {
+    useFirestore()
+})
+
+const handleSubmit = () => {
+    v$.value.$touch()
+    if (v$.value.$invalid) {
+        return
+    }
+    isSubmitting.value = true
+    error.value = ''
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user)
+        isSubmittedSuccessfully.value = true
+        successMessage.value = 'Login successful! Welcome back.'
+    })
+    .catch((e) => {
+        console.log(e.code)
+        error.value = e.message.replace('Firebase: ', '');
+    })
+    .finally(() => {
+        // keep disabled if success; re-enable if there was an error
+        if (!isSubmittedSuccessfully.value) {
+            isSubmitting.value = false
+        }
+    });
+
+}
+</script>
+
